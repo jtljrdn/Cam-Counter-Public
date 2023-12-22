ccff9343eaedc4e23d937251a99851deaa15ef03
Jordan <jtlee@rmgj2.com>
2023-12-21 18:21:21 -0600

    Remove invite link from help command

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -8,7 +8,7 @@ module.exports = {
     const helpEmbed = new EmbedBuilder()
       .setColor("#0099ff")
       .setTitle("Help")
-      .setDescription("**Useful Links**\n[GitHub](https://github.com/jtljrdn/cam-counter-public)\n[Invite](https://discord.com/api/oauth2/authorize?client_id=1186507379173503137&permissions=76800&scope=bot+applications.commands)\n[Community Discord](https://discord.gg/bDwKqSreue)")
+      .setDescription("**Useful Links**\n[GitHub](https://github.com/jtljrdn/cam-counter-public)\n[Community Discord](https://discord.gg/bDwKqSreue)")
       .addFields(
         { name: "**Commands**", value: "\n"},
         { name: "/add", value: "Add to the count" },
