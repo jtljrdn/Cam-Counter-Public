54cf772a0d0260499d8477734ceee129144eb6da
Jordan <jtlee@rmgj2.com>
2024-01-01 20:38:32 -0600

    Updated user command

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,14 +1,49 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { logErrors } = require("../../logging");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user")
-    .setDescription("Provides information about the user."),
+    .setDescription("Provides information about the user.")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("User").setRequired(true)
+    ),
   async execute(interaction) {
-    // interaction.user is the object representing the User who ran the command
-    // interaction.member is the GuildMember object, which represents the user in the specific guild
-    await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
-    );
+    try {
+      const guildUser = await interaction.guild.members.fetch(interaction.options.getUser("user"));
+      const createdAt = new Date(interaction.options.getUser("user").createdAt.getTime())
+      const userEmbed = new EmbedBuilder()
+        .setTitle(`${interaction.user.username} Info`)
+        .setImage(guildUser.user.displayAvatarURL())
+        .setColor("Random")
+        .addFields(
+          {
+            name: "Username",
+            value: interaction.options.getUser("user").username,
+          },
+          {
+            name: "User ID",
+            value: interaction.options.getUser("user").id,
+          },
+          {
+            name: "Created At",
+            value: `<t:${parseInt((createdAt.getTime()/1000).toFixed(0))}>`,
+          },
+          {
+            name: "Joined At",
+            value: `<t:${parseInt((guildUser.joinedTimestamp/1000).toFixed(0))}>`,
+          },
+          {
+            name: "Roles",
+            value: guildUser.roles.cache.map((role) => `${role}`).join(" | "),
+          }
+        )
+        .setTimestamp();
+      await interaction.reply({ embeds: [userEmbed] });
+    } catch (error) {
+      logErrors(interaction, error);
+      console.log(error);
+      interaction.reply("Error getting user info. Join the support server for help:\nhttps://discord.gg/bDwKqSreue.");
+    }
   },
 };
