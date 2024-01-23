33d8b616f4ee95fb288a43f64199c66030b33404
Jordan <jtlee@rmgj2.com>
2024-01-23 16:56:47 -0600

    Add game search functionality using RAWG API

--- commands/fun/game-search.js
+++ commands/fun/game-search.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { logErrors } = require("../../logging");
+const { default: axios } = require("axios");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,8 +14,41 @@ module.exports = {
     ),
   async execute(interaction) {
     try {
-    await interaction.reply("RESPONSE");
+      await interaction.deferReply({ ephemeral: true });
+      const game = interaction.options.getString("game");
+      const { data: gameData, status } = await axios.get(
+        `https://api.rawg.io/api/games?key=${process.env.RAWGAPI_KEY}&search=${game}`
+      );
+      const embed = new EmbedBuilder().setTitle("Game Search");
+      if (status !== 200 || gameData.count === 0) {
+        embed.setDescription("Something went wrong. Check your spelling and try again.").setColor("Red");
+        await interaction.editReply({ embeds: [embed] });
+        return;
+      }
+      const gameInfo = gameData.results[0];
+      const platforms = gameInfo.platforms
+        .map((platform) => platform.platform.name)
+        .join(", ");
+      const rating = gameInfo.metacritic || "N/A";
+      const genres = gameInfo.genres.map((genre) => genre.name).join(", ");
+      const stores = gameInfo.stores.map((store) => store.store.name).join(", ");
+      embed
+        .setTitle(gameInfo.name)
+        .addFields(
+          { name: "Released", value: gameInfo.released },
+          { name: "Available On", value: stores.length > 0 ? stores : "N/A"},
+          { name: "Platforms", value: platforms },
+          { name: "Metascore", value: `${rating}` },
+          { name: "Genres", value: genres }
+        )
+        .setImage(gameInfo.background_image)
+        .setColor(gameInfo.dominant_color)
+        .setTimestamp()
+        .setFooter({ text: "Some info may be inaccurate | Powered by RAWG.io" })
+        .setURL(`https://rawg.io/games/${gameInfo.slug}`);
+      await interaction.editReply({ embeds: [embed], ephemeral: false});
     } catch (error) {
+      await interaction.editReply("Something went wrong.");
       logErrors(interaction, error);
       console.log(error);
     }
